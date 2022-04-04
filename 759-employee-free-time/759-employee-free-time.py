a9daba6353e268216b421935d483f370d470defc
Stephane <57733711+remigere@users.noreply.github.com>
2022-04-04 16:07:02 -0700

    Time: 96 ms (84.97%), Space: 15.9 MB (42.70%) - LeetHub

---
+++ 759-employee-free-time/759-employee-free-time.py
@@ -0,0 +1,19 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        ints = sorted([i for s in schedule for i in s], key=lambda x: x.start)
+        res, pre = [], ints[0]
+        for i in ints[1:]:
+            if i.start <= pre.end and i.end > pre.end:
+                pre.end = i.end
+            elif i.start > pre.end:
+                res.append(Interval(pre.end, i.start))
+                pre = i
+        return res
