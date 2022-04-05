6f7124531f71d5bb74d73ed061d1ad665747bc41
Stephane <57733711+remigere@users.noreply.github.com>
2022-04-05 10:21:31 -0700

    Time: 1805 ms (64.89%), Space: 14.2 MB (15.93%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -4,7 +4,7 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
-
+        """
         for i in range(n):
             found = True
             for j in range(n):
@@ -15,3 +15,15 @@ class Solution:
             if found:
                 return i
         return -1
+        """
+        prec = 0
+        for i in range(1, n):
+            if knows(prec, i):
+                prec = i
+        found = True
+        for j in range(n):
+            if j != prec:
+                if not(knows(j, prec)) or knows(prec, j):
+                    found = False
+                    return - 1
+        return prec
