d8e7276fbb75c75d2577a00d6144a44ffc1a002c
Stephane <57733711+remigere@users.noreply.github.com>
2022-04-13 11:04:17 -0700

    Time: 413 ms (65.19%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -2,7 +2,7 @@
 SELECT 
     id AS Id, 
     CASE 
-        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
+        WHEN tree.p_id is NULL
             THEN "Root"
         WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
             THEN "Inner"
