cf8e4735f6fd32c50d58af8e4d7f4511e7b784dd
Stephane <57733711+remigere@users.noreply.github.com>
2022-04-17 09:37:14 -0700

    Time: 636 ms (50.16%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-select person1, person2, count(duration) as call_count, sum(duration) as total_duration
-from (
-select if(from_id > to_id, to_id, from_id) as person1,
-    if(from_id > to_id, from_id, to_id) as person2,
-    duration
-    from Calls) as sub
+select if(from_id > to_id, to_id, from_id) as person1, 
+    if(from_id > to_id, from_id, to_id) as person2, 
+    count(duration) as call_count, 
+    sum(duration) as total_duration
+from Calls
 group by person1, person2
