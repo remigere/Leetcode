9b1fe00eefa815a98daff076cd5fd0bd9cafaf8c
Stephane <remigere@stanford.edu>
2022-05-05 17:05:14 -0700

    Time: 5836 ms (76.17%), Space: 14 MB (53.07%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,39 +1,21 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        self.ROWS = len(board)
-        self.COLS = len(board[0])
-        self.board = board
-
-        for row in range(self.ROWS):
-            for col in range(self.COLS):
-                if self.backtrack(row, col, word):
-                    return True
-
-        # no match found after all exploration
-        return False
-
-
-    def backtrack(self, row, col, suffix):
-        # bottom case: we find match for each letter in the word
-        if len(suffix) == 0:
-            return True
-
-        # Check the current status, before jumping into backtracking
-        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
-                or self.board[row][col] != suffix[0]:
+    def exist(self, board, word):
+        if not board:
             return False
-
-        ret = False
-        # mark the choice before exploring further.
-        self.board[row][col] = '#'
-        # explore the 4 neighbor directions
-        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
-            # break instead of return directly to do some cleanup afterwards
-            if ret: break
-
-        # revert the change, a clean slate and no side-effect
-        self.board[row][col] = suffix[0]
-
-        # Tried all directions, and did not find any match
-        return ret
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+    
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:
+            return True
+        if i < 0 or i > len(board) - 1 or j < 0 or j > len(board[0]) - 1 or board[i][j] != word[0]:
+            return False
+        tmp = board[i][j]
+        board[i][j] = "#"
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
