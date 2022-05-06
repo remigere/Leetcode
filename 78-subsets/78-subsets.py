d48ccfe20b93bcea1dd74eb977b3262c27fa0d10
Stephane <remigere@stanford.edu>
2022-05-05 17:48:11 -0700

    Time: 30 ms (96.02%), Space: 14.1 MB (81.63%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,12 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = [[]]
-        
-        for num in nums:
-            new = []
-            for cur in output:
-                new.append(cur + [num])
-            output.extend(new)
-        
-        return output
+        ans = []
+        def recursion(nums, cur_list):
+            if len(nums) == 0:
+                ans.append(cur_list)
+            else:
+                recursion(nums[1:], cur_list)
+                recursion(nums[1:], cur_list + [nums[0]])
+        recursion(nums, [])
+        return ans
