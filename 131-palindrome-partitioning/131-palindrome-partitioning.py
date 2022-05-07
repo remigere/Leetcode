c98040d97d38f6b85751b1e8c5f1f60392284311
Stephane <remigere@stanford.edu>
2022-05-06 23:37:41 -0700

    Time: 1160 ms (12.56%), Space: 29 MB (87.60%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,42 +1,14 @@
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:
-
-        """
-        def ispalindrome(s):
-            for i in range(len(s) // 2):
-                if s[i] != s[- i - 1]:
-                    return False
-            return True
-        
-        output = []  
-        n = len(s)
-        
-        def backtracking(comb, cur, start, l):
-            if start == n:
-                if l == n:
-                    output.append(list(comb))
+    def partition(self, s):
+        def dfs(s, path, res):
+            if not s:
+                res.append(path[:])
                 return
-            else:
-                new = cur + s[start]
-                if ispalindrome(new):
-                    backtracking(comb + [new], "", start + 1, l + len(new))
-                backtracking(comb, new, start + 1, l)
-        
-        backtracking([], "", 0, 0)
-        return output
-    
-        """
+            for i in range(1, len(s)+1):
+                if s[:i] == s[i-1::-1]:
+                    path.append(s[:i])
+                    dfs(s[i:], path, res)
+                    path.pop()        
         res = []
-        self.dfs(s, [], res)
+        dfs(s, [], res)
         return res
-    
-    def dfs(self, s, path, res):
-        if not s:
-            res.append(path)
-            return
-        for i in range(1, len(s)+1):
-            if self.isPal(s[:i]):
-                self.dfs(s[i:], path+[s[:i]], res)
-
-    def isPal(self, s):
-        return s == s[::-1]
