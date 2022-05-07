e98cff730fa63e7510154573e43793c73cc8af56
Stephane <remigere@stanford.edu>
2022-05-07 00:47:46 -0700

    Time: 232 ms (46.33%), Space: 21.4 MB (18.98%) - LeetHub

--- 320-generalized-abbreviation/320-generalized-abbreviation.py
+++ 320-generalized-abbreviation/320-generalized-abbreviation.py
@@ -5,21 +5,14 @@ class Solution:
         def backtracking(comb, start, cur):
             if start == n:
                 if cur:
-                    output.append(str(comb) + str(cur))
-                else:
-                    output.append(str(comb))
+                    comb += str(cur)
+                output.append(str(comb))
                 return
             else:
-                if start == 0:
-                    backtracking(comb, start + 1, 1)
-                    backtracking(comb + word[start], start + 1, 0)
-                else:
-                    if cur:
-                        backtracking(comb + str(cur) + word[start], start + 1, 0)
-                        backtracking(comb, start + 1, cur + 1)
-                    else:
-                        backtracking(comb + word[start], start + 1, 0)
-                        backtracking(comb, start + 1, 1)
+                backtracking(comb, start + 1, cur + 1)
+                if cur:
+                    comb += str(cur)
+                backtracking(comb + word[start], start + 1, 0)
             
         backtracking("", 0, 0)
         return output
