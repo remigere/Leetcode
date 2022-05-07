55d34a613dd5da2a31ebf6f82a6bb651cc4996a2
Stephane <remigere@stanford.edu>
2022-05-06 23:36:19 -0700

    Time: 1060 ms (20.49%), Space: 29.1 MB (79.96%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,7 +1,7 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
 
-        
+        """
         def ispalindrome(s):
             for i in range(len(s) // 2):
                 if s[i] != s[- i - 1]:
@@ -24,3 +24,19 @@ class Solution:
         
         backtracking([], "", 0, 0)
         return output
+    
+        """
+        res = []
+        self.dfs(s, [], res)
+        return res
+    
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path)
+            return
+        for i in range(1, len(s)+1):
+            if self.isPal(s[:i]):
+                self.dfs(s[i:], path+[s[:i]], res)
+
+    def isPal(self, s):
+        return s == s[::-1]
