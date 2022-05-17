3bfd278ed71eafb0c905c8819ab068b5dd841ee4
Stephane <remigere@stanford.edu>
2022-05-17 16:54:43 -0700

    Time: 1400 ms (5.00%), Space: 36.3 MB (20.22%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -1,27 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def merge(self, h1, h2):
-        dummy = tail = ListNode(None)
-        while h1 and h2:
-            if h1.val < h2.val:
-                tail.next, tail, h1 = h1, h1, h1.next
-            else:
-                tail.next, tail, h2 = h2, h2, h2.next
-    
-        tail.next = h1 or h2
-        return dummy.next
-    
+class Solution(object):
     def sortList(self, head):
         if not head or not head.next:
             return head
-    
-        pre, slow, fast = None, head, head
+        fast, slow = head.next, head
         while fast and fast.next:
-            pre, slow, fast = slow, slow.next, fast.next.next
-        pre.next = None
-
-        return self.merge(*map(self.sortList, (head, slow)))
+            fast = fast.next.next
+            slow = slow.next
+        start = slow.next
+        slow.next = None
+        l, r = self.sortList(head), self.sortList(start)
+        return self.merge(l, r)
+        
+        
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        dummy = p = ListNode(0)
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
+        return dummy.next
