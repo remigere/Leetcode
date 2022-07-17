a991b8cae43b546f27b8bccfc05a97435289ff47
Stephane <remigere@stanford.edu>
2022-07-16 23:29:54 -0700

    Time: 198 ms (22.67%), Space: 15.4 MB (67.23%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,35 @@
+class Union:
+    def __init__(self, n):
+        self.n = n
+        self.rank = [0] * n
+        self.root = [i for i in range(n)]
+    
+    def find(self, x):
+        while self.root[x] != x:
+            x = self.root[x]
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            self.n -= 1
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+    
+    def count(self):
+        return self.n
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        #union-find
+        uf = Union(n)
+        for edge in edges:
+            uf.union(edge[0], edge[1])
+        return uf.count()
