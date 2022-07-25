6ba0188d09f9d7a83e4b6ca7f919ed110df168fd
Stephane <remigere@stanford.edu>
2022-07-24 23:47:11 -0700

    Time: 170 ms (14.97%), Space: 15.4 MB (93.21%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,33 +2,31 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         start, end = 0, len(nums) - 1
         
-        def binary_search(start, end, target):
+        def binary_search(start, end, target, pos):
             mid = (end + start) // 2
             while start <= end:
                 mid = (start + end) // 2
-                if nums[mid] == target and (mid == len(nums) - 1 or nums[mid] < nums[mid + 1]):
-                    return mid
-                elif nums[mid] <= target:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-            return -1
-        
-        def binary_search2(start, end, target):
-            mid = (end + start) // 2
-            while start <= end:
-                mid = (start + end) // 2
-                if nums[mid] == target and (mid == 0 or nums[mid] > nums[mid - 1]):
-                    return mid
+                if nums[mid] == target:
+                    if pos == "last":
+                        
+                        if (mid == len(nums) - 1 or nums[mid] < nums[mid + 1]):
+                            return mid
+                        start = mid + 1
+                        
+                    elif pos == "first":
+                        
+                        if (mid == 0 or nums[mid] > nums[mid - 1]):
+                            return mid
+                        end = mid - 1
+                        
                 elif nums[mid] < target:
                     start = mid + 1
+                    
                 else:
                     end = mid - 1
             return -1
-        
-        return [binary_search2(start, end, target), binary_search(start, end, target)]
-        print(binary_search(start, end, target))
-        return
+       
+        return [binary_search(start, end, target, "first"), binary_search(start, end, target, "last")]
             
                 
                 
