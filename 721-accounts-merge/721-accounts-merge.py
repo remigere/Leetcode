be5c17ff386f63967565b6ac07fb484f0a36c8f1
Stephane <remigere@stanford.edu>
2022-07-26 21:26:14 -0700

    Time: 224 ms (86.71%), Space: 17.8 MB (90.15%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,40 @@
+class UF:
+    def __init__(self, n):
+        self.n = n
+        self.count = n
+        self.root = [i for i in range(n)]
+        self.rank = [0 for i in range(n)]
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootY] += 1
+            self.count -= 1
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UF(len(accounts))
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
