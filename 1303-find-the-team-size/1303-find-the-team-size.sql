3f20f15077a968e982a7873840e537c86ae6304b
Stephane <remigere@stanford.edu>
2022-07-27 12:28:59 -0700

    Time: 389 ms (34.26%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,3 +1,21 @@
 # Write your MySQL query statement below
-select e.employee_id, (select count(team_id) from Employee where e.team_id = team_id) as team_size
-from Employee e
+
+
+# select e.employee_id, (select count(team_id) from Employee where team_id = e.team_id) as team_size
+# from Employee as e
+
+# SELECT employee_id, 
+#     COUNT(team_id) OVER(PARTITION BY team_id) AS team_size
+# FROM Employee
+
+# select employee_id, count(*) over(partition by team_id) as team_size
+# from employee
+
+SELECT e.employee_id, team_size
+FROM Employee AS e
+LEFT JOIN (
+      SELECT team_id, COUNT(employee_id) AS team_size
+      FROM Employee
+      GROUP BY team_id
+) AS teams
+ON e.team_id = teams.team_id
