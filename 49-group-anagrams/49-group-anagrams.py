12afafc86627559f27516079f9ad1ca12b9d9541
Stephane <remigere@stanford.edu>
2022-07-26 18:18:37 -0700

    Time: 184 ms (32.84%), Space: 19.9 MB (9.84%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,8 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if not strs:
-            return [[""]]
+        # if not strs:
+        #     return [[""]]
+        # d = defaultdict(list)
+        # for word in strs:
+        #     d[str(sorted(word))].append(word)
+        # return d.values()
+    
+
         d = defaultdict(list)
         for word in strs:
-            d[str(sorted(word))].append(word)
-        return [anagrams for anagrams in d.values()]
+            key = [0] * 26
+            for char in word:
+                key[ord(char) - ord("a")] += 1
+            d[tuple(key)].append(word)
+        return d.values()
