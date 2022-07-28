748587eb70c43fb640dca79980ba5c5753896dfe
Stephane <remigere@stanford.edu>
2022-07-27 21:04:27 -0700

    Time: 76 ms (84.62%), Space: 13.9 MB (86.51%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -7,8 +7,8 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         res = ListNode()
         cur = res
-        add = 0
-        while l1 or l2:
+        carry = 0
+        while l1 or l2 or carry:
             s = 0
             if l1:
                 s += l1.val
@@ -16,9 +16,7 @@ class Solution:
             if l2:
                 s += l2.val
                 l2 = l2.next
-            cur.next = ListNode((s + add) % 10)
-            add = (s + add) // 10
+            cur.next = ListNode((s + carry) % 10)
+            carry = (s + carry) // 10
             cur = cur.next
-        if add:
-            cur.next = ListNode(add)
         return res.next
