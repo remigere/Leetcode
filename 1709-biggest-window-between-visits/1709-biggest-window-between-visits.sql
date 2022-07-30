dca0b8fdc49f898a44d4a95aa7d2c2e49df68f3c
Stephane <remigere@stanford.edu>
2022-07-30 12:23:03 -0700

    Time: 457 ms (92.45%), Space: 0B (100.00%) - LeetHub

--- 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
+++ 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
@@ -3,12 +3,13 @@
 with temp as
 (
 select user_id, 
-    - datediff(visit_date, lead(visit_date, 1, '2021-01-01') over(partition by user_id order by user_id, visit_date)) as dif
+    - datediff(visit_date, lead(visit_date, 1, '2021-01-01') over(partition by user_id order by visit_date)) as dif
 from UserVisits
-order by user_id, visit_date
+#order by user_id, visit_date
 )
 
 
-select distinct user_id, dif as biggest_window
+select distinct user_id, max(dif) as biggest_window
 from temp t
-where dif = (select max(dif) from temp where user_id = t.user_id)
+group by user_id
+order by user_id
